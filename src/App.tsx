741d98cb0405d6d0711bfaca6c59c0ecd52c3c69
celiahouessou-star <celiahouessou@gmail.com>
2026-05-03 20:39:05 +0200

    fix routing github pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* 🔥 FIX GITHUB PAGES */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
